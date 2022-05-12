2ce2f6777f35842311328a80e122605c154dfac0
Angel Navarrete Sanchez <anavarrete18@ilg.cat>
2022-05-12 18:31:36 +0200

    Aplicacion con Usuarios Autorizados

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.get('/users', isAuthorized,(req,res) => {
+  res.json([{
+    id: 1,
+    name: 'User Userson'
+  }])
+})
+
 app.get("/products", (req,res) => {
   const products = [
   {
@@ -23,5 +30,15 @@ app.get("/products", (req,res) => {
 
  res.json(products);
 });
+
+function isAuthorized(req,res, next) {
+  const auth = req.headers.authorization;
+  if (auth === 'secretpassword') {
+    next();
+  } else {
+    res.status(401);
+    res.send('Not permitted');
+  }
+}
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
